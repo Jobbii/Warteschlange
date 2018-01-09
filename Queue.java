f65d96a7602486ce342dfdf10478ddd9d73833c6
Fabian <fabianjobmann0@gmail.com>
2018-01-09 09:18:43 +0100

    Initial sharing of project

---
+++ Queue.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of interface Queue here.
+ *
+ * @author (paul Mentz)
+ * @version (09.01.18)
+ */
+public interface Queue
+{
+    /**
+     * An example of a method header - replace this comment with your own
+     *
+     * @param  y a sample parameter for a method
+     * @return   the result produced by sampleMethod
+     */
+    int sampleMethod(int y);
+}
